2bc8da891475fc974a5ea08922374497190b8809
Lance Bragstad <lbragstad@gmail.com>
2017-09-05 15:43:31 +0000

    Modify run.sh for testing policy tracking

--- run.sh
+++ run.sh
@@ -17,6 +17,20 @@ source .venv/bin/activate
 #exit $?
 
 sed -i "s/Last updated:.*/Last updated: $(date -u)/" index.html
-git add data.* *.json index.html
-git commit -m "Updated csv"
-scp -i ~/.ssh/id_rsa-backups data.* *.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
+
+# FIXME(lbragstad): Use this eventually to keep results in source controller
+# and updated automatically.
+# git add data.* *.json index.html
+# git commit -m "Updated csv"
+
+# FIXME(lbragstad): Switch back to this eventually once I figure out a way to
+# integrate this into my blog, instead of Doug's.
+# scp -i ~/.ssh/id_rsa-backups data.* *.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
+
+# NOTE(lbragstad): Locally testing only. Be explicit in copying files to expose
+# since I can't seem to find a way to implement missing_urls.json for the
+# policy work.
+sudo cp data.csv /var/www/html/policy/
+sudo cp data.json /var/www/html/policy/
+sudo cp notstarted.json /var/www/html/policy/
+sudo cp index.html /var/www/html/policy/
