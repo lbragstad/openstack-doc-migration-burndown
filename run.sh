e3db1d3894d994a44f9c143974d9fde17c2854dc
Lance Bragstad <lbragstad@gmail.com>
2017-09-06 15:32:38 +0000

    Remove scp commands
    Instead of using scp to transfer datafiles to the host I'm using
    rawgit to link to the repository from my blog.

--- run.sh
+++ run.sh
@@ -18,19 +18,5 @@ source .venv/bin/activate
 
 sed -i "s/Last updated:.*/Last updated: $(date -u)/" index.html
 
-# FIXME(lbragstad): Use this eventually to keep results in source controller
-# and updated automatically.
 git add data.* *.json index.html
 git commit -m "Updated csv"
-
-# FIXME(lbragstad): Switch back to this eventually once I figure out a way to
-# integrate this into my blog, instead of Doug's.
-# scp -i ~/.ssh/id_rsa-backups data.* *.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
-
-# NOTE(lbragstad): Locally testing only. Be explicit in copying files to expose
-# since I can't seem to find a way to implement missing_urls.json for the
-# policy work.
-sudo cp data.csv /var/www/html/policy/
-sudo cp data.json /var/www/html/policy/
-sudo cp notstarted.json /var/www/html/policy/
-sudo cp index.html /var/www/html/policy/
